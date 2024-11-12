d010c1df451cd01ccd0fc8409ef528531b9a6a3f
nifaev <nifaev.ivan@gmail.com>
2024-11-12 12:08:47 +0300

    Добавлены дополнительные тесты

--- tests/certification_testing/test_all_project.py
+++ tests/certification_testing/test_all_project.py
@@ -43,7 +43,7 @@ def test_circle_fit_into_noone_figures():
     assert g.fit_into_figure(Circle(3)) == []
 
 
-def test_triangle_fit_into_three_figures():
+def test_triangle_fit_into_three_figures_success():
     g = Geometry()
     g.add_figure(Circle(3))
     g.add_figure(Rectangle(3, 5))
@@ -52,19 +52,46 @@ def test_triangle_fit_into_three_figures():
     assert result == [True, True, True]
 
 
-def test_rectangle_fit_into_three_figures():
+def test_triangle_fit_into_three_figures_failure():
+    g = Geometry()
+    g.add_figure(Circle(3))
+    g.add_figure(Rectangle(3, 5))
+    g.add_figure(Triangle(3, 4, 5))
+    result = g.fit_into_figure(Triangle(6, 8, 10))
+    assert result == [False, False, False]
+
+
+def test_rectangle_fit_into_three_figures_success():
     g = Geometry()
     g.add_figure(Circle(3))
     g.add_figure(Rectangle(2, 4))
     g.add_figure(Triangle(3, 4, 5))
-    result = g.fit_into_figure(Rectangle(5, 2))
-    assert result == [True, False, False]
+    result = g.fit_into_figure(Rectangle(1, 1))
+    assert result == [True, True, True]
 
 
-def test_circle_fit_into_three_figures():
+def test_rectangle_fit_into_three_figures_failure():
+    g = Geometry()
+    g.add_figure(Circle(3))
+    g.add_figure(Rectangle(2, 4))
+    g.add_figure(Triangle(3, 4, 5))
+    result = g.fit_into_figure(Rectangle(10, 2))
+    assert result == [False, False, False]
+
+
+def test_circle_fit_into_three_figures_success():
     g = Geometry()
     g.add_figure(Circle(3))
     g.add_figure(Rectangle(2, 4))
     g.add_figure(Triangle(3, 4, 5))
     result = g.fit_into_figure(Circle(1))
     assert result == [True, True, True]
+
+
+def test_circle_fit_into_three_figures_failure():
+    g = Geometry()
+    g.add_figure(Circle(3))
+    g.add_figure(Rectangle(2, 4))
+    g.add_figure(Triangle(3, 4, 5))
+    result = g.fit_into_figure(Circle(5))
+    assert result == [False, False, False]
